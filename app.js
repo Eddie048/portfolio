187f6815b50d3512af4d1f7ca653ce5b12706357
Eddie Stump <ejstump13@gmail.com>
2023-05-11 19:04:04 -0400

    Refactored js file

--- app.js
+++ app.js
@@ -9,13 +9,14 @@ var aboutSection = document.getElementById("about");
  * When the navbar is scrolled past, it is set to be stuck to the top of the screen and the space is filled by adjusting the padding of the about section
  */
 function updateNavBar() {
-  if (window.pageYOffset >= navbarY) {
-    navbar.classList.add("sticky");
-    aboutSection.style.marginTop = navBarHeight + "px";
-  } else {
-    navbar.classList.remove("sticky");
-    aboutSection.style.marginTop = "0px";
-  }
+
+    if (window.pageYOffset >= navbarY) {
+        navbar.classList.add("sticky");
+        aboutSection.style.marginTop = navBarHeight + "px";
+    } else {
+        navbar.classList.remove("sticky");
+        aboutSection.style.marginTop = "0px";
+    }
 }
 
 window.onscroll = function() {updateNavBar()};
