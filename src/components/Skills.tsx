6da58d10ce54bdf259f21ebbf3e39a3c68baa78a
Eddie Stump <ejstump13@gmail.com>
2024-06-09 21:43:47 -0400

    made skills page responsive

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,7 +3,7 @@ import skills from "../assets/index.tsx";
 
 const Skills = () => {
   return (
-    <div className="w-full h-screen bg-slate-900">
+    <div className="w-full h-fit min-h-screen bg-slate-900 content-center">
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full gap-3">
         <h2 className="text-gray-300 font-bold text-xl border-b-2 border-pink-600 w-fit pr-4">
           My Skills
@@ -13,7 +13,7 @@ const Skills = () => {
           science clubs, classes, and hobby projects. Here are some of the
           languages, frameworks, and tools I've learned along the way.
         </p>
-        <div className="grid grid-cols-5 gap-10 justify-items-center mx-auto">
+        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-10 justify-items-center mx-auto mb-10">
           {skills.map(({ name, icon }) => (
             <div className="bg-slate-700 w-fit p-3 hover:border-pink-300 duration-700 dur hover:scale-110 hover:shadow-lg hover:shadow-sky-400">
               <img
