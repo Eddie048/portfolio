3ee4476db2631613ef54282f3dcc18aea4c89bbd
Eddie Stump <ejstump13@gmail.com>
2024-06-14 12:23:59 -0400

    changed App from jsx to tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import NoMatch from "./pages/NoMatch.tsx";
+import React from "react";
 
 function App() {
   return (
