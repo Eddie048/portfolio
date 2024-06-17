f659c8762c1e7a848563a2e8e8ad0fa7855711a0
Eddie Stump <ejstump13@gmail.com>
2024-06-17 14:38:22 -0400

    added content to planey-physics page

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,6 +1,7 @@
 import hospital_login from "./hospital-application/hospital-app-login.png";
 import website_icon from "./portfolio-website/website-icon.png";
 import planet_physics_demo from "./planet-physics/planet-physics-demo.mov";
+import planet_physics_code from "./planet-physics/planet-physics-code.png";
 import hypixel_player_tracker_demo from "./hypixel-player-tracker/hypixel_player_tracker_output.png";
 import poker_output_3 from "./poker-project/poker_output_3.png";
 import juststance_demo from "./threading-problems/juststance_demo.png";
@@ -25,16 +26,24 @@ const projects: project[] = [
     projectTitle: "Planet Physics",
     pageAddress: "planet-physics",
     description:
-      "As my first personal project early in high school, I used equations from my physics class to create an orbital mechanics simulation.",
+      "As a fun side project during high school, I used equations from an AP physics class to create a 2D orbital mechanics simulation.",
     display_media: planet_physics_demo,
     content: [
       {
         type: "text",
-        data: "As my first personal project early in high school, I used equations from my physics class to create an orbital mechanics simulation.",
+        data: "During high school, I was taking a physics class and a Java class at the same time. In physics, I learned about the 3 body problem, which says that in a system of just 3 gravitational objects, there is no closed form solution - there is no way to write an equation that gives the exact movements of the 3 objects. In addition, the behavior of the objects is often chaotic. Because we had just started learning about graphics in my Java class, I decided to simulate orbital motion using Java. This is a demonstartion of several objects of various mass moving around each other.",
       },
       {
         type: "video",
         data: planet_physics_demo,
+      },
+      {
+        type: "text",
+        data: "This was a thoroughly enjoyable project to work on, as I was able to combine my two favorite subjects and create something visually interesting. My favorite part was using the gravitational equations I learned in physics, changing them into a format that I could put into code using knowledge from math classes, and then writing the equations using the Math library in Java. The following image is the update function that contains the majority of the math for the program, the part I am most proud of.",
+      },
+      {
+        type: "image",
+        data: planet_physics_code,
       },
     ],
   },
