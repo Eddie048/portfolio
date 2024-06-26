3e6bae94832472e224a61369709c30ad27378a61
Eddie Stump <ejstump13@gmail.com>
2024-06-25 22:53:22 -0400

    Fixed Vercel Analytics

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { Analytics } from '@vercel/analytics/react';
+import { inject } from '@vercel/analytics';
+
+inject();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
-      <Analytics />
     </BrowserRouter>
   </React.StrictMode>
 );
