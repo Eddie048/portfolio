b85f19370c39016b295970fe2af8d12830feea25
Eddie Stump <ejstump13@gmail.com>
2024-06-25 23:20:12 -0400

    added speed insights from Vercel

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
+// Inject analytics and speed insights scripts from Vercel
 inject();
+injectSpeedInsights();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
