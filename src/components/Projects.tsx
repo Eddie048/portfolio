e710d3cec4390e20adade5624a7a204ee7533999
Eddie Stump <ejstump13@gmail.com>
2024-07-03 10:57:32 -0400

    fixed coloring issue on homepage

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,25 +7,18 @@ const Projects = () => {
     <div id="projects" className="page-section flex flex-col gap-3">
       <h2 className="title">Projects</h2>
       <p className="text-text text-lg">
-        Explore some of the projects I've worked on over the years. Click/tap or
-        hover the images to learn more!
+        Explore some of the projects I've worked on over the years. Click, tap,
+        or hover on the images to explore.
       </p>
       <div className="grid grid-cols-1 lg:grid-cols-2 justify-items-center mx-auto gap-14 mt-6">
         {projects.map(
-          ({
-            projectTitle,
-            description,
-            display_media,
-            pageAddress,
-            links,
-          }) => (
+          ({ projectTitle, description, display_media, pageAddress }) => (
             <Card
               key={projectTitle}
               title={projectTitle}
               description={description}
               media_src={display_media}
               pageAddress={pageAddress}
-              links={links}
             />
           )
         )}
